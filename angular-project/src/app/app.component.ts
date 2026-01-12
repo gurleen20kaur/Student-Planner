0aebef284a54dcfd07dad1a46f964b442cc875d0
gurleen20kaur <gkwahla@mun.ca>
2026-01-11 22:49:15 -0330

    Added Tabs successfully, navigation between tabs through rendering html tags in the App view

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { TaskListComponent } from './task-list/task-list.component';
+import { CalendarViewComponent } from './calendar-view/calendar-view.component';
+import { WeeklyViewComponent } from './weekly-view/weekly-view.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TaskListComponent, CalendarViewComponent, WeeklyViewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -13,6 +16,7 @@ export class AppComponent {
 
   switchTab(tab: string) {
     this.currentTab = tab;
+    console.log(`Switched to ${tab} view`);
   }
 
 }
--- angular-project/src/app/app.routes.ts
+++ angular-project/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { TaskListComponent } from './task-list/task-list.component';
+import { CalendarViewComponent } from './calendar-view/calendar-view.component';
+import { WeeklyViewComponent } from './weekly-view/weekly-view.component';
 
 
 export const routes: Routes = [
-  { path: '', component: TaskListComponent }
+  { path: 'Todo', component: TaskListComponent },
+  { path: 'Calendar', component: CalendarViewComponent },
+  { path: 'WeeklySchedule', component: WeeklyViewComponent }
 ];
