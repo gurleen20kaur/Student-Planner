e937d99208bf891879a5d8776150983b743e0ea4
gurleen20kaur <gkwahla@mun.ca>
2026-01-21 22:30:53 -0330

    Added DashBoard, and Router

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { TaskListComponent } from './task-list/task-list.component';
 import { CalendarViewComponent } from './calendar-view/calendar-view.component';
 import { WeeklyViewComponent } from './weekly-view/weekly-view.component';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TaskListComponent, CalendarViewComponent, WeeklyViewComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive,TaskListComponent, CalendarViewComponent, WeeklyViewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- angular-project/src/app/app.routes.ts
+++ angular-project/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { TaskListComponent } from './task-list/task-list.component';
 import { CalendarViewComponent } from './calendar-view/calendar-view.component';
 import { WeeklyViewComponent } from './weekly-view/weekly-view.component';
-
+import { DashBoardComponent } from './dash-board/dash-board.component'; 
 
 export const routes: Routes = [
-  { path: 'Todo', component: TaskListComponent },
-  { path: 'Calendar', component: CalendarViewComponent },
-  { path: 'WeeklySchedule', component: WeeklyViewComponent }
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '', component: DashBoardComponent },
+  { path: 'todo', component: TaskListComponent },
+  { path: 'calendar', component: CalendarViewComponent },
+  { path: 'weekly-schedule', component: WeeklyViewComponent }
 ];
